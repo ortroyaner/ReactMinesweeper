b3631a03ee79dcb476ff852b8b34b3d692785a21
ortroyaner <or.troyaner@gmail.com>
2020-04-15 00:33:27 +0300

    Updates

---
+++ precache-manifest.d30c17c14982406ed2b629c14f2c951a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fdf23ad429b29738834c6b58dd6d32",
+    "url": "/reactMinesweeper/index.html"
+  },
+  {
+    "revision": "8b77604a827f53d2e0cf",
+    "url": "/reactMinesweeper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de55ffdcf9b9ea76e5bb",
+    "url": "/reactMinesweeper/static/js/2.b9c1736c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactMinesweeper/static/js/2.b9c1736c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b77604a827f53d2e0cf",
+    "url": "/reactMinesweeper/static/js/main.24475521.chunk.js"
+  },
+  {
+    "revision": "e39b5f54cde3b4fb646b",
+    "url": "/reactMinesweeper/static/js/runtime-main.d3447a92.js"
+  }
+]);
